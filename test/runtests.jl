892126c8d6d08c8d3c1220384522fc3ad9a473a8
Romeo Valentin <romeov@stanford.edu>
2025-07-30 19:10:29 -0700

    Don't test everything immediately

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ using StaticArrays
 @testset "RunwayPoseEstimation.jl" begin
     include("unit/test_coordinate_systems.jl")
     include("unit/test_camera_model.jl")
-    include("unit/test_data_management.jl")
+    # include("unit/test_data_management.jl")
     include("unit/test_pose_estimation.jl")
-    include("unit/test_uncertainty_quantification.jl")
-    include("unit/test_integrity_monitoring.jl")
-    include("unit/test_visualization.jl")
+    # include("unit/test_uncertainty_quantification.jl")
+    # include("unit/test_integrity_monitoring.jl")
+    # include("unit/test_visualization.jl")
 
     include("integration/test_end_to_end.jl")
 end
