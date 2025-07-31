cb878a4adfb050b6e93dc581a4683b5bb9d13d78
Romeo Valentin <romeov@stanford.edu>
2025-07-30 17:59:28 -0700

    Major rewrite to move to precompiled caches
    This enables trimming and is inspired by the design in `TrimSimpleNonlinearSolve.jl`.

--- src/pose_estimation/optimization.jl
+++ src/pose_estimation/optimization.jl
@@ -8,12 +8,17 @@ noise models.
 
 using SimpleNonlinearSolve
 import Moshi.Match: @match
+# import NonlinearSolveBase
+import NonlinearSolveFirstOrder
 import NonlinearSolveFirstOrder: LevenbergMarquardt, NewtonRaphson, TrustRegion
 import SciMLBase: successful_retcode
 using StaticArrays
 using LinearAlgebra
 import LinearAlgebra: AbstractTriangular
 using Rotations
+using LinearSolve
+const LS = LinearSolve
+
 
 abstract type AbstractPoseOptimizationParams end
 
@@ -103,17 +108,14 @@ function pose_optimization_objective(optvar::AbstractVector{T},
     errors = reduce(vcat, error_vectors)
 
     # Apply noise weighting via Cholesky decomposition
-    U = ps.chol_upper * 1pixel
-    L_inv = inv(U')
+    # U = ps.chol_upper * 1pixel
+    # L_inv = inv(U')
+    L_inv = ps.chol_upper / 1pixel
+
     weighted_errors = L_inv * errors
     
     return ustrip.(NoUnits, weighted_errors)
 end
-
-const POSEOPTFN = NonlinearFunction{false}(pose_optimization_objective)
-const AD = AutoForwardDiff(; chunksize=1)
-# const ALG = LevenbergMarquardt(; autodiff=AD)
-const ALG = TrustRegion(; autodiff=AD)
 
 const _default6dofnoisemodel(pts) =
     UncorrGaussianNoiseModel(
@@ -121,6 +123,62 @@ const _default6dofnoisemodel(pts) =
                              Normal(0.0, 2.0)]
                           for _ in pts])
     )
+const POSEOPTFN = NonlinearFunction{false, SciMLBase.FullSpecialize}(pose_optimization_objective)
+const AD = AutoForwardDiff(; chunksize=1)
+const ALG = LevenbergMarquardt(; autodiff=AD, linsolve=LS.CholeskyFactorization())
+# const ALG = TrustRegion(; autodiff=AD, linsolve=LS.CholeskyFactorization())
+# const ALG = NewtonRaphson(; autodiff=AD, linsolve=LS.RFLUFactorization())
+# const ALG = NewtonRaphson(; autodiff=AD, linsolve=LS.DirectLdiv!())
+# const ALG = TrustRegion(; autodiff=AD)
+# const ALG = SimpleNewtonRaphson(; autodiff=AD)
+const PROB6DOF = let
+        runway_corners = SA[
+            WorldPoint(1000.0m, -50.0m, 0.0m),
+            WorldPoint(1000.0m, 50.0m, 0.0m),
+            WorldPoint(3000.0m, 50.0m, 0.0m),
+            WorldPoint(3000.0m, -50.0m, 0.0m)
+        ]
+        true_pos = WorldPoint(-1300.0m, 0.0m, 80.0m)
+        true_rot = RotZYX(roll=0.03, pitch=0.04, yaw=0.05)
+        projections = [project(true_pos, true_rot, corner, CAMERA_CONFIG_OFFSET) for corner in runway_corners]
+    noise_model = _default6dofnoisemodel(projections)
+    ps = PoseOptimizationParams6DOF(
+        runway_corners, projections,
+        CAMERA_CONFIG, inv(cholesky(covmatrix(noise_model)).U)
+    )
+    # NonlinearLeastSquaresProblem{false}(POSEOPTFN, SVector{6}(rand(6)), ps)
+    NonlinearLeastSquaresProblem{false}(POSEOPTFN, rand(6), ps)
+end
+const CACHE6DOF = init(PROB6DOF, ALG)
+
+const _default3dofnoisemodel(pts) =
+    UncorrGaussianNoiseModel(
+            reduce(vcat, [SA[Normal(0.0, 2.0),
+                             Normal(0.0, 2.0)]
+                          for _ in pts])
+    )
+const PROB3DOF = let
+        runway_corners = SA[
+            WorldPoint(1000.0m, -50.0m, 0.0m),
+            WorldPoint(1000.0m, 50.0m, 0.0m),
+            WorldPoint(3000.0m, 50.0m, 0.0m),
+            WorldPoint(3000.0m, -50.0m, 0.0m)
+        ]
+        true_pos = WorldPoint(-1300.0m, 0.0m, 80.0m)
+        true_rot = RotZYX(roll=0.03, pitch=0.04, yaw=0.05)
+        projections = [project(true_pos, true_rot, corner, CAMERA_CONFIG_OFFSET) for corner in runway_corners]
+    noise_model = _default3dofnoisemodel(projections)
+    ps = PoseOptimizationParams3DOF(
+        runway_corners, projections,
+        CAMERA_CONFIG, inv(cholesky(covmatrix(noise_model)).U),
+        true_rot
+    )
+
+    # NonlinearLeastSquaresProblem{false}(POSEOPTFN, SVector{3}(rand(3)), ps)
+    NonlinearLeastSquaresProblem{false}(POSEOPTFN, rand(3), ps)
+end
+const CACHE3DOF = init(PROB3DOF, ALG)
+
 function estimatepose6dof(
         runway_corners::AbstractVector{<:WorldPoint},
         observed_corners::AbstractVector{<:ProjectionPoint{T, S}},
@@ -135,27 +193,32 @@ function estimatepose6dof(
 
     ps = PoseOptimizationParams6DOF(
         runway_corners, observed_corners,
-        config, cholesky(covmatrix(noise_model)).U)
+        config, inv(cholesky(covmatrix(noise_model)).U))
 
-    prob = NonlinearLeastSquaresProblem{false}(POSEOPTFN, u₀, ps)
+    # prob = NonlinearLeastSquaresProblem{false}(POSEOPTFN, u₀, ps)
+    # prob = remake(PROB6DOF; u₀, p=ps)
+    # prob = NonlinearLeastSquaresProblem{false}(POSEOPTFN, SVector{6}(rand(6)), ps)
+    # prob = NonlinearLeastSquaresProblem{false}(POSEOPTFN, [-1000.0; 0; 100; 0; 0; 0], ps)
+    # ALG = TrustRegion(; autodiff=AD, linsolve=LS.CholeskyFactorization())
+    # ALG = LevenbergMarquardt(; autodiff=AD, linsolve=LS.CholeskyFactorization())
+    # ALG = LevenbergMarquardt(; autodiff=AD, linsolve=LS.RFLUFactorization(; thread=Val(false)))
+    # CACHE6DOF = init(prob, ALG)
+    NonlinearSolveFirstOrder.reinit!(CACHE6DOF, collect(u₀); p=ps)
+    NonlinearSolveFirstOrder.solve!(CACHE6DOF)
+    sol = (; u=CACHE6DOF.u, retcode=CACHE6DOF.retcode)
+
     # termination_condition = AbsNormSafeBestTerminationMode(
     #     Base.Fix2(norm, 2);
     #     max_stalled_steps = 32)
 
-    sol = solve(prob, ALG)
+    # sol = solve(prob, ALG)
 
-    !successful_retcode(sol) && throw(OptimizationFailedError(sol.retcode, sol))
+    !successful_retcode(sol.retcode) && throw(OptimizationFailedError(sol.retcode, sol))
     pos = WorldPoint(sol.u[1:3]m)
     rot = RotZYX(roll=sol.u[4]rad, pitch=sol.u[5]rad, yaw=sol.u[6]rad)
     return (; pos, rot)
 end
 
-const _default3dofnoisemodel(pts) =
-    UncorrGaussianNoiseModel(
-            reduce(vcat, [SA[Normal(0.0, 2.0),
-                             Normal(0.0, 2.0)]
-                          for _ in pts])
-    )
 function estimatepose3dof(
         runway_corners::AbstractVector{<:WorldPoint},
         observed_corners::AbstractVector{<:ProjectionPoint{T, S}},
@@ -170,18 +233,21 @@ function estimatepose3dof(
 
     ps = PoseOptimizationParams3DOF(
         runway_corners, observed_corners,
-        config, cholesky(covmatrix(noise_model)).U, known_attitude)
+        config, inv(cholesky(covmatrix(noise_model)).U), known_attitude)
 
-
-    prob = NonlinearLeastSquaresProblem{false}(POSEOPTFN, u₀, ps)
+    # prob = NonlinearLeastSquaresProblem{false}(POSEOPTFN, u₀, ps)
+    # prob = remake(PROB3DOF; collect(u₀), p=ps)
     # alg = LevenbergMarquardt()
     # termination_condition = AbsNormSafeBestTerminationMode(
     #     Base.Fix2(norm, 2);
     #     max_stalled_steps = 32)
+    NonlinearSolveFirstOrder.reinit!(CACHE3DOF, collect(u₀); p=ps)
+    NonlinearSolveFirstOrder.solve!(CACHE3DOF)
+    sol = (; u=CACHE3DOF.u, retcode=CACHE3DOF.retcode)
 
-    sol = solve(prob, ALG)
+    # sol = solve(prob, ALG)
 
-    !successful_retcode(sol) && throw(OptimizationFailedError(sol.retcode, sol))
+    !successful_retcode(sol.retcode) && throw(OptimizationFailedError(sol.retcode, sol))
     pos = WorldPoint(sol.u[1:3]m)
     rot = known_attitude
     return (; pos, rot)
