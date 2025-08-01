d46ec3700cbba91a6bf5f4d52f54b485715d4623
Romeo Valentin <romeov@stanford.edu>
2025-07-31 23:48:53 -0700

    Add `juliac` directory

---
+++ juliac/main.jl
@@ -0,0 +1,32 @@
+using Distributions
+using LinearAlgebra
+using RunwayLib
+using ProbabilisticParameterEstimators
+using Rotations
+using Unitful, Unitful.DefaultSymbols
+using StaticArrays
+
+function (@main)(args::Vector{String})::Cint
+    runway_corners = SA[
+        WorldPoint(0.0m, -50.0m, 0.0m),
+        WorldPoint(0.0m, 50.0m, 0.0m),
+        WorldPoint(3000.0m, 50.0m, 0.0m),
+        WorldPoint(3000.0m, -50.0m, 0.0m),
+    ]
+    true_pos = WorldPoint(-1300.0m, 0.0m, 80.0m)
+    true_rot = RotZYX(roll = 0.03, pitch = 0.04, yaw = 0.05)
+    camconfig = CAMERA_CONFIG_OFFSET
+    projections = [project(true_pos, true_rot, corner, camconfig) for corner in runway_corners]
+
+    sol6dof = estimatepose6dof(
+        runway_corners, projections, camconfig
+    )
+
+    sol3dof = estimatepose3dof(
+        runway_corners, projections, true_rot, camconfig
+    )
+    println(Core.stdout, ustrip(m, sol6dof.pos.x))
+    println(Core.stdout, ustrip(m, sol6dof.pos.y))
+    println(Core.stdout, ustrip(m, sol6dof.pos.z))
+    return 0
+end
